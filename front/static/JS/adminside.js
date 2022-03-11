c0de1e31c936f6edaf26566d3aa546d73230d049
ilyada <s341853@oslomet.no>
2022-03-11 16:35:29 +0100

    Lagt til liste i modalen

--- front/static/JS/adminside.js
+++ front/static/JS/adminside.js
@@ -1,25 +1,32 @@
+$(document).ready(function(){
+	$("#avstemning_button").click(function(){
+		let markedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+		if( markedCheckboxes.length == 0){ 
+			alert("ingen bokser er checked")
+			return
+		}
+		else{
+			$("#myModal").modal();
+			formaterTitler()
+		}
+	});
+});
 
+options = []
 function createPoll(){
-	options = []
 	let markedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-	if( markedCheckboxes.length == 0){ 
-		alert("ingen bokser er checked")
-		console.log("ingen bokser er checked")
-		$('#myModal').hide()
-		location.reload() 
-		return
-	}
-	
 	for(cb of markedCheckboxes){
 		//per nå legger vi bare til video linken, men senere skal vi legge til et helt option object
 		//et option object inkluderer tittel på mål, tekst beskrivelse, og video link
-		options.push(createOption(cb.dataset.vidlink))
+		options.push(createOption(cb.dataset.title, cb.dataset.vidlink))
 	}
+	console.log(options)
 	//oppretter et poll object
 	let poll = {"options" : options, "title": "this is a poll"}
 	
 	//poster poll objectet til backend
 	postSetPoll(poll)
+	getTitles()
 	$('#myModal').hide()
 	location.reload()  
 	return
@@ -27,8 +34,36 @@ function createPoll(){
 
 //Denne metoden skal senere ta inn tittel og text i tillegg
 //Returnerer et json object
-function createOption(link){
-	return {"video_url":link}
+function createOption(title, link){
+	return {"title" : title, 
+			"video_url" : link}
+}
+
+//Denne metoden printer ut og returnerer kun tittelen på options. skal bli brukt til å liste ut videoene i modalen.
+function getTitles(){
+	for(a of options){
+		console.log(a.title)
+		return a.title
+	}
+}
+
+//Denne må endres slik at mål tittelen blir hentet fra options array.
+//Nå er det hardkodet at den kjører 3 ganger og skriver "mål 1" som tittel
+function formaterTitler(){
+	let hei = "HEIHEI"
+	let table = 
+		"<div class='list-group'>"
+	for(var i = 0; i < 3; i++){
+		table+=
+		"<a href='#' class='list-group-item list-group-item-action flex-column align-items-start'>" +
+		"<div class='d-flex w-100 justify-content-between'>" + 
+		"<h5 class='mb-1'>Mål1</h5>" + 
+		"</div>"
+		"</a>"
+	}
+	table+=
+	"</div>"
+	$("#valgteVideoer").html(table)
 }
 
 
