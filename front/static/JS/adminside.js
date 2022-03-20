8afc6bdc819ca9e1f4d62c5f9d4934f005fcc541
ilyada <s341853@oslomet.no>
2022-03-20 14:08:39 +0100

    Endret lista i modalen til å ikke være knapper

--- front/static/JS/adminside.js
+++ front/static/JS/adminside.js
@@ -45,11 +45,11 @@ function formaterTitler(){
 		"<div class='list-group'>"
 	for(a of options){
 		table+=
-		"<a href='#' class='list-group-item list-group-item-action flex-column align-items-start'>" +
+		"<li class='list-group-item flex-column align-items-start'>" +
 		"<div class='d-flex w-100 justify-content-between'>" + 
 		"<h5 class='mb-1'>"+a.title+"</h5>" + 
 		"</div>"
-		"</a>"
+		"</li>"
 	}
 	table+=
 	"</div>"
