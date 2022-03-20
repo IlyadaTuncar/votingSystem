fde4c6fea3358918e716a79b378c6c43dcf47838
Edem Agbo <edem.s.agbo@gmail.com>
2022-03-20 14:26:25 +0100

    leger til videoer dynamisk

--- front/static/JS/fanside.js
+++ front/static/JS/fanside.js
@@ -11,36 +11,29 @@ function updateVideoist(newList){
 // må lage en funksjon her med en for løkke som oppretter videospilleren, og linken ved hjelp av html kode, akk som i admin
 
 
-function openVideo(event, videoNr){
+function openVideo(event, video_url){
 	var videoElm = videojs("videoPlayer");
-    //console.log(videoNr)
-
+	
 	if(!videoElm.paused()){
-		//console.log("pause")
 		videoElm.pause()
 	}
-	if (videoNr=="video1"){
-		//console.log("kjører if fra video 1")
-		videoElm.src(videoList[0])
-	} else if(videoNr=="video2"){
-		//console.log("kjører if fra video 2")
-		videoElm.src(videoList[1])
-	} else if(videoNr=="video3"){
-		//console.log("kjører if fra video 3")
-		videoElm.src(videoList[2])
-	}
+	videoElm.src(video_url)
 	videoElm.play()
+	
+}
+
+function formaterOptions(options){
+	let table = '<div class="button-container">' 
+	for(o of options){
+		table += '<button class="tablinks" onclick="openVideo(event, '+"'"+o.video_url+"'"+')">'+o.title+'</button>'
+	}
+	table += '</div>'
+	$(".videoTab").html(table)
 }
 
 function getOptions(){
 	$.get('/get_options', function ( data ){	
-		newVideos = []
-		for(a of data){
-			for(b of a.options){
-				newVideos.push(b.video_url)
-			}
-		}
-		updateVideoist(newVideos);
+		formaterOptions(data[0].options)
 	});
 }
 
