235b8f36d79e17342d8c20ec01a84f48c77e7565
harun <s331449@oslomet.no>
2022-03-21 13:38:03 +0100

    La til thumbnail_url i options.

--- front/static/JS/adminside.js
+++ front/static/JS/adminside.js
@@ -1,75 +1,79 @@
-$(document).ready(function(){
-	getapi();
-	$("#avstemning_button").click(function(){
-		let markedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-		if( markedCheckboxes.length == 0){ 
-			alert("ingen bokser er checked")
-			return
-		}
-		else{
-			options = []
-			let markedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-			for(cb of markedCheckboxes){
-				//per nå legger vi bare til video linken, men senere skal vi legge til et helt option object
-				//et option object inkluderer tittel på mål, tekst beskrivelse, og video link
-				options.push(createOption(cb.dataset.title, cb.dataset.vidlink, cb.dataset.dato, cb.dataset.motstander))
-			}
-			formaterTitler()
-			$("#myModal").modal();
-		}
-	});
+$(document).ready(function() {
+    getapi();
+    $("#avstemning_button").click(function() {
+        let markedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+        if (markedCheckboxes.length == 0) {
+            alert("ingen bokser er checked")
+            return
+        } else {
+            options = []
+            let markedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+            for (cb of markedCheckboxes) {
+                //per nå legger vi bare til video linken, men senere skal vi legge til et helt option object
+                //et option object inkluderer tittel på mål, tekst beskrivelse, og video link
+                options.push(createOption(cb.dataset.title, cb.dataset.vidlink, cb.dataset.dato, cb.dataset.motstander,
+                    cb.dataset.thumbnail))
+            }
+            formaterTitler()
+            $("#myModal").modal();
+        }
+    });
 });
 
 options = []
-function createPoll(){
-	const pollTittel = document.getElementById("pollTittel").value;
-	const pollBeskrivelse = document.getElementById("pollBeskrivelse").value;
-	//oppretter et poll object
-	let poll = {"title": pollTittel, "poll_description": pollBeskrivelse, "options" : options}
-	//poster poll objectet til backend
-	postSetPoll(poll)
-	//$('#myModal').hide()
-	location.reload()  
-	return
+
+function createPoll() {
+    const pollTittel = document.getElementById("pollTittel").value;
+    const pollBeskrivelse = document.getElementById("pollBeskrivelse").value;
+    //oppretter et poll object
+    let poll = { "title": pollTittel, "poll_description": pollBeskrivelse, "options": options }
+        //poster poll objectet til backend
+    postSetPoll(poll)
+        //$('#myModal').hide()
+    location.reload()
+    return
 }
 
 //Denne metoden skal senere ta inn tittel og text i tillegg
 //Returnerer et json object
-function createOption(title, link, dato, motstander){
-	return {"title" : title, 
-			"video_url" : link,
-			"dato" : dato,
-			"motstander": motstander}
+function createOption(title, link, dato, motstander, thumbnail) {
+    return {
+        "title": title,
+        "video_url": link,
+        "dato": dato,
+        "motstander": motstander,
+        "thumbnail_url": thumbnail
+    }
 }
 
-function formaterTitler(){
-	let table = 
-		"<div class='list-group'>"
-	for(a of options){
-		table+=
-		"<li class='list-group-item flex-column align-items-start'>" +
-		"<div class='d-flex w-100 justify-content-between'>" + 
-		"<h5 class='mb-1'>"+a.title+"</h5>" + 
-		"</div>"
-		"</li>"
-	}
-	table+=
-	"</div>"
-	$("#valgteVideoer").html(table)
+function formaterTitler() {
+    let table =
+        "<div class='list-group'>"
+    for (a of options) {
+        table +=
+            "<li class='list-group-item flex-column align-items-start'>" +
+            "<div class='d-flex w-100 justify-content-between'>" +
+            "<h5 class='mb-1'>" + a.title + "</h5>" +
+            "</div>"
+        "</li>"
+    }
+    table +=
+        "</div>"
+    $("#valgteVideoer").html(table)
 }
 
 
-function postSetPoll(poll){
-	$.ajax({
-		url:'/create_poll',
-		method:"POST",
-		data:JSON.stringify(poll),
-		contentType:"application/json; charset=utf-8",
-		dataType:"json",
-		success: function(){
-			//console.log('Success')
-		}
-	})
+function postSetPoll(poll) {
+    $.ajax({
+        url: '/create_poll',
+        method: "POST",
+        data: JSON.stringify(poll),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function() {
+            //console.log('Success')
+        }
+    })
 }
 
 //Her prøver jeg å hente videoer fra api og formatere videoene
@@ -79,33 +83,33 @@ async function getapi() {
     const response = await fetch(api_url);
     const data = await response.json();
 
-	let table = 
-		"<div class='content-block row'>" +
-		"<div class='container'>" +
-		"<div class='row'>"
-		for(a of data.playlists){
-			for(b of a.events){
-				for(c of b.tags){
-					table+=
-					"<div class='col-sm'>"+
-					"<input type='checkbox' id='option1' name='option1' data-dato='"+a.game.date+"' data-motstander='"+a.game.visiting_team.name+"' data-title='"+a.description+"' data-vidlink='"+a.video_url+"'></input>" +
-					"<label id='optionLabel' for='option1'>Velg</label>" +
-					"<video id='my-video' class='video-js vjs-big-play-centered' controls preload='auto' width='320' height='180' poster='"+a.thumbnail_url+"' data-setup='{}'>"+
-					"<source src='"+a.video_url+"' type='application/x-mpegURL' />"+
-					"<p class='vjs-no-js'> To view this video please enable JavaScript, and consider upgrading to a web browser that" + 
-					"<a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>" +
-					"</p>" + 
-					"</video>" + 
-					"<br>"+
-					"<h6 id='optionNavn'>Scoring av&nbsp"+c.scorer.value+"&nbspmot&nbsp"+a.game.visiting_team.name+"&nbsp&nbsp"+a.game.date+"</h6>"+
-					"<script src='https://vjs.zencdn.net/7.17.0/video.min.js'></script>" + 
-					"</div>"
-				}
-			}
-		}
-		table+= "</div>" +
-		"</div>"+
-		"</div>"
+    let table =
+        "<div class='content-block row'>" +
+        "<div class='container'>" +
+        "<div class='row'>"
+    for (a of data.playlists) {
+        for (b of a.events) {
+            for (c of b.tags) {
+                table +=
+                    "<div class='col-sm'>" +
+                    "<input type='checkbox' id='option1' name='option1' data-dato='" + a.game.date + "' data-motstander='" + a.game.visiting_team.name + "' data-title='" + a.description + "' data-vidlink='" + a.video_url + "' data-thumbnail='" + a.thumbnail_url + "'></input>" +
+                    "<label id='optionLabel' for='option1'>Velg</label>" +
+                    "<video id='my-video' class='video-js vjs-big-play-centered' controls preload='auto' width='320' height='180' poster='" + a.thumbnail_url + "' data-setup='{}'>" +
+                    "<source src='" + a.video_url + "' type='application/x-mpegURL' />" +
+                    "<p class='vjs-no-js'> To view this video please enable JavaScript, and consider upgrading to a web browser that" +
+                    "<a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>" +
+                    "</p>" +
+                    "</video>" +
+                    "<br>" +
+                    "<h6 id='optionNavn'>Scoring av&nbsp" + c.scorer.value + "&nbspmot&nbsp" + a.game.visiting_team.name + "&nbsp&nbsp" + a.game.date + "</h6>" +
+                    "<script src='https://vjs.zencdn.net/7.17.0/video.min.js'></script>" +
+                    "</div>"
+            }
+        }
+    }
+    table += "</div>" +
+        "</div>" +
+        "</div>"
 
-		$("#nyesteVideoer").html(table)
+    $("#nyesteVideoer").html(table)
 }
