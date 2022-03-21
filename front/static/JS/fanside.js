5a893a74dc50d60a630bc3f86860d1ad1d6e45cd
AlexTran98 <56254619+AlexTran98@users.noreply.github.com>
2022-03-21 01:02:49 +0100

    Fortsatt noe problemer på hvorfor det modalen ikke kommer opp. Sjekker imorgen

--- front/static/JS/fanside.js
+++ front/static/JS/fanside.js
@@ -38,3 +38,39 @@ function getPoll(id){
 		formaterOptions(data.options)
 	});
 }
+
+$(document).ready(function(){
+	getapi();
+	$("#stem_button").click(function(){
+		let markedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+		if( markedCheckboxes.length == 0){
+			alert("ingen bokser er checked")
+			return
+		}
+		else{
+			options = []
+			let markedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+			for(cb of markedCheckboxes){
+				//per nå legger vi bare til video linken, men senere skal vi legge til et helt option object
+				//et option object inkluderer tittel på mål, tekst beskrivelse, og video link
+				options.push(createOption(cb.dataset.title, cb.dataset.vidlink, cb.dataset.dato, cb.dataset.motstander))
+			}
+			formaterTitler()
+			$("#myModal").modal();
+		}
+	});
+});
+
+options = []
+function createVote(){
+	const stemNavn = document.getElementById("stemNavn").value;
+	const stemEpost = document.getElementById("stemEpost").value;
+	const stemBeskrivelse = document.getElementById("stemBeskrivelse").value;
+	//oppretter et poll object
+	let poll = {"navn": stemNavn, "Epost": stemEpost, "vote_description": stemBeskrivelse, "options" : options}
+	//poster poll objectet til backend
+	postSetPoll(poll)
+	//$('#myModal').hide()
+	location.reload()
+	return
+}
