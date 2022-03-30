aac95bbeddfaf6d3c05263afd8ffaa7c770cd540
ilyada <s341853@oslomet.no>
2022-03-30 14:08:07 +0200

    La til ordentlige verdier i databasen

--- back/SQL_scripts/test.sql
+++ back/SQL_scripts/test.sql
@@ -43,22 +43,31 @@ CREATE TABLE TBL_VOTE (
 
 
 INSERT INTO TBL_POLL (Client_id, poll_title, poll_description, sluttdato)
-VALUES (45, 'Goal of the season', 'Which one of these three goals is the best of the season', '01-06-2022');
+VALUES (45, 'Goal of the season', 'Which one of these three goals is the best of the season', '01-12-2022');
 
 INSERT INTO TBL_POLL (Client_id, poll_title, poll_description, sluttdato)
-VALUES (30, 'Goal of the week', 'Which one of these eight goals is the best of this week', '21-05-2022');
+VALUES (30, 'Goal of the week', 'Which one of these eight goals is the best of this week', '21-12-2022');
 
 INSERT INTO TBL_OPTION (poll_id, option_title, video_url, dato, scorer, scorerlag, motstander, thumbnail_url)
-VALUES (2, 'Mikel Obi v LSK', 'vid url', '01-02-2022', 'OBI', 'RBK', 'LSK', 'thumbnail_url');
+VALUES (2, 'Mål Odd! Scoring av Tobias Lauritsen.', 'https://api.forzasys.com/eliteserien/playlist.m3u8/2962:6778000:6813000/Manifest.m3u8', '2021-12-12', 'Tobias Lauritsen', 'Odd', 'Stabæk', 'https://d22hh18o76pkhl.cloudfront.net/mediabank/thumb/eliteserien/2962/06788.jpg');
 
 INSERT INTO TBL_OPTION (poll_id, option_title, video_url, dato, scorer, scorerlag, motstander, thumbnail_url)
-VALUES (1, 'Marcus Abrahamsen v Odd', 'vid url', '01-03-2022', 'Abrahamsen', 'B/G', 'ODD', 'thumbnail_url');
+VALUES (2, 'Mål Odd! Scoring av Espen Ruud.', 'https://api.forzasys.com/eliteserien/playlist.m3u8/2962:6340000:6375000/Manifest.m3u8', '2021-12-12', 'Espen Ruud', 'Odd', 'Stabæk', 'https://d22hh18o76pkhl.cloudfront.net/mediabank/thumb/eliteserien/2962/06350.jpg');
 
 INSERT INTO TBL_OPTION (poll_id, option_title, video_url, dato, scorer, scorerlag, motstander, thumbnail_url)
-VALUES (1, 'Marcus Abrahamsen v Vif', 'vid url', '01-02-2022', 'Abrahamsen', 'B/G', 'VIF', 'thumbnail_url');
+VALUES (2, 'Mål Stabæk! Scoring av Fredrik Haugen.', 'https://api.forzasys.com/eliteserien/playlist.m3u8/2962:5767000:5802000/Manifest.m3u8', '2021-12-12', 'Fredrik Haugen', 'Stabæk', 'Odd', 'https://d22hh18o76pkhl.cloudfront.net/mediabank/thumb/eliteserien/2962/05777.jpg');
+
+INSERT INTO TBL_OPTION (poll_id, option_title, video_url, dato, scorer, scorerlag, motstander, thumbnail_url)
+VALUES (1, 'Mål Haugesund! Scoring av Alexander Toft Søderlund.', 'https://api.forzasys.com/eliteserien/playlist.m3u8/2966:5399000:5434000/Manifest.m3u8', '2021-12-12', 'Alexander Toft Søderlund', 'Haugesund', 'Molde', 'https://d22hh18o76pkhl.cloudfront.net/mediabank/thumb/eliteserien/2966/05409.jpg');
+
+INSERT INTO TBL_OPTION (poll_id, option_title, video_url, dato, scorer, scorerlag, motstander, thumbnail_url)
+VALUES (1, 'Mål Molde! Scoring av Ola Brynhildsen.', 'https://api.forzasys.com/eliteserien/playlist.m3u8/2966:5001000:5036000/Manifest.m3u8', '2021-12-12', 'Ola Brynhildsen', 'Molde', 'Molde', 'https://d22hh18o76pkhl.cloudfront.net/mediabank/thumb/eliteserien/2966/05011.jpg');
+
+INSERT INTO TBL_OPTION (poll_id, option_title, video_url, dato, scorer, scorerlag, motstander, thumbnail_url)
+VALUES (1, 'Mål Odd! Scoring av Tobias Lauritsen.', 'https://api.forzasys.com/eliteserien/playlist.m3u8/2962:6778000:6813000/Manifest.m3u8', '2021-12-12', 'Tobias Lauritsen', 'Odd', 'Stabæk', 'https://d22hh18o76pkhl.cloudfront.net/mediabank/thumb/eliteserien/2962/06788.jpg');
 
 INSERT INTO TBL_VOTE (option_id, email) 
-VALUES (1, 'jklsdf123@email.com');
+VALUES (1, 'olaNordmann@gmail.com');
 
 INSERT INTO TBL_VOTE (option_id, email) 
-VALUES (2, 'sdfg234@gmail.com');
+VALUES (2, 'kariNordmann@hotmail.com');
